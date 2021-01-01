6030df8eba74d58cc6138ba87a55ab4ba31f1f8a
Giltwizy <giltonbosco@yahoo.com>
2021-01-01 16:21:50 +0300

    Added symbols

--- password-generator.py
+++ password-generator.py
@@ -2,3 +2,11 @@ import random
 import string
 
 LETTERS = string.ascii_letters
+NUMS = '0123456789'
+SPE = '-+*%&$!_'
+SYMBOLS = LETTERS+NUMS+SPE
+
+len = int(input("Enter pass length:"))
+password = "".join(random.sample(SYMBOLS,len))
+
+print(password)
